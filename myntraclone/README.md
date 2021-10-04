e57ae896fdb43c1dec933600f734279dedcf1356
Monalika14 <85168004+Monalika14@users.noreply.github.com>
2021-10-04 22:08:13 +0530

    Add files via upload

---
+++ myntraclone/README.md
@@ -0,0 +1,32 @@
+# myntra-clone
+
+## Project UI
+
+
+https://user-images.githubusercontent.com/58461801/134894661-bb1ba02c-36bf-47a2-98c6-87b2089ce184.mp4
+
+
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ myntraclone/src/components/NavBar.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="navbar">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <ul>
+      <router-link to="/"
+        ><img class="logo" src="../assets/myntra-logo.png" alt=""
+      /></router-link>
+      <li>
+        <router-link :to="{ name: 'category', params: { category: men } }"
+          >{{ $t("main.men") }}
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'category', params: { category: women } }"
+          >{{ $t("main.women") }}
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          :to="{ name: 'category', params: { category: 'electronics' } }"
+          >{{ $t("main.electronics") }}
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          :to="{ name: 'category', params: { category: 'jewelery' } }"
+          >{{ $t("main.jewelery") }}
+        </router-link>
+      </li>
+    </ul>
+    <!-- <p>{{ loggedUser.name }}</p> -->
+    <router-link :to="{ name: 'login' }" class="loginRegister"
+      >Login/Signup</router-link
+    >
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      men: "men's clothing",
+      women: "women's clothing",
+    };
+  },
+};
+</script>
+<style scoped>
+.navbar {
+  height: 80px;
+  box-shadow: 0 4px 12px 0 rgb(0 0 0 / 5%);
+}
+ul {
+  list-style-type: none;
+  display: flex;
+  width: 40%;
+  justify-content: space-around;
+  font-weight: 700;
+  padding: 0;
+  font-size: 15px;
+}
+.logo {
+  height: 60px;
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+li {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+.loginRegister {
+  margin-right: 20px;
+  font-weight: 700;
+  font-size: 20px;
+  padding: 10px;
+}
+.loginRegister:hover {
+  border: 2px solid #ff3f6c;
+}
+</style>
